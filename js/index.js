85247a4d3936e204c7e375fb76bc1ba0b7daba4e
Jochem Kuijpers <github.com@jochemkuijpers.nl>
2016-06-07 17:14:36 +0200

    Working current and target temperature meters

--- js/index.js
+++ js/index.js
@@ -3,31 +3,36 @@ var tempCurrent = 0;
 var tempTarget = 0;
 
 function updateCurrentTemperature() {
-	var tempNewCurrent = get("currentTemperature", "current_temperature");
+	var tempNewCurrent = parseFloat(get("currentTemperature", "current_temperature"));
 	
-	var changeIcon = "";
 	if (tempNewCurrent > tempCurrent) {
-		changeIcon = "up";
-	}
-	if (tempNewCurrent < tempCurrent) {
-		changeIcon = "down";
+		$("#temp-current-icon")
+			.removeClass("glyphicon-arrow-down glyphicon-minus")
+			.addClass("glyphicon-arrow-up")
+	} else if (tempNewCurrent < tempCurrent) {
+		$("#temp-current-icon")
+			.removeClass("glyphicon-arrow-up glyphicon-minus")
+			.addClass("glyphicon-arrow-down")
+	} else {
+		$("#temp-current-icon")
+			.removeClass("glyphicon-arrow-up glyphicon-arrow-down")
+			.addClass("glyphicon-minus")
 	}
 	
-	if (changeIcon != "") {
-		
-	}
-	$("#tempInput").val(tempNewCurrent);
+	tempCurrent = tempNewCurrent;
 	$("#temp-current").html(tempNewCurrent);
-}
-
-function initialize() {
-
 }
 
 //function to put the target temperature, onClick also a heating indicator will be displayed.
 $(document).ready(function () {
 	var updateCurrentTempInterval = setInterval(updateCurrentTemperature, 1000);
-	$("#temp-target").html(get("targetTemperature", "target_temperature"));
+	
+	tempCurrent = parseFloat(get("currentTemperature", "current_temperature"));
+	$("#temp-current").html(tempCurrent);
+	
+	tempTarget = parseFloat(get("targetTemperature", "target_temperature"));
+	$("#temp-target").html(tempTarget);
+	$("#tempInput").val(tempTarget);
 	
 	$("#tempInput").focus(function() {
 		// disallow selection of the input field
@@ -37,6 +42,6 @@ $(document).ready(function () {
     $('#setTempBtn').click(function () {
         var targetTemp = $("#tempInput").val();
         put("targetTemperature", "target_temperature", targetTemp);
-        $('"#temp-target"').html(targetTemp);
+        $("#temp-target").html(targetTemp);
     });
 });
