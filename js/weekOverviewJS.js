546804f0f0dbd0de25e742ee289c4a72c6398f9a
koenyyy <koen_de_raad@hotmail.com>
2016-06-19 15:48:37 +0200

    Switches toevoegen en deleten mogelijk

--- js/weekOverviewJS.js
+++ js/weekOverviewJS.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     //get the weekprogram
     var weekProgram = getWeekProgram();
-    var dayTemperature = get("dayTemperature", "day_temperature");
+    var dayTemperature = get('dayTemperature', 'day_temperature');
     var weekProgramString = JSON.stringify(weekProgram);
     var obj = $.parseJSON(weekProgramString);
 
@@ -13,86 +13,85 @@ $(document).ready(function () {
     //go through all days from json xml thing and display the according switches
     $.each(obj, function (index, value) {
         if (index == 'Monday') {
-            for (i = 0; i < obj['Monday'].length; i++) {
-                var value = obj['Monday'][i].toString().replace(",", " - ");
-
-                $('#MondayContent').append('<p>' + value + " at " + dayTemperature + "°C" + '<p>');
-            }
             //check if there are already 5 switches if so, no button to add is displayed
             if (obj['Monday'].length < 5) {
-                $('#MondayContent').append('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+                $('#MondayContent').prepend('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+            }
+
+            for (i = 0; i < obj['Monday'].length; i++) {
+                var value = obj['Monday'][i].toString().replace(",", " - ");
+                $('#MondayContent').prepend('<div class="removeEdit"><p>' + value + " at " + dayTemperature + "°C" + '</p><span id="'+i+'" class="btn btn-primary removeBtn"><img src="img/garbage.png" width="50%"></span></div>');
             }
         }
 
         if (index == 'Tuesday') {
-            for (i = 0; i < obj['Tuesday'].length; i++) {
-                var value = obj['Tuesday'][i].toString().replace(",", " - ");
-
-                $('#TuesdayContent').append('<p>' + value + " at " + dayTemperature + "°C" + '<p>');
-            }
             //check if there are already 5 switches if so, no button to add is displayed
             if (obj['Tuesday'].length < 5) {
-                $('#TuesdayContent').append('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+                $('#TuesdayContent').prepend('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+            }
+            for (i = 0; i < obj['Tuesday'].length; i++) {
+                var value = obj['Tuesday'][i].toString().replace(",", " - ");
+                $('#TuesdayContent').prepend('<p>' + value + " at " + dayTemperature + "°C" + '</p>');
             }
         }
 
         if (index == 'Wednesday') {
+            //check if there are already 5 switches if so, no button to add is displayed
+            if (obj['Wednesday'].length < 5) {
+                $('#WednesdayContent').prepend('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+            }
             for (i = 0; i < obj['Wednesday'].length; i++) {
                 var value = obj['Wednesday'][i].toString().replace(",", " - ");
 
-                $('#WednesdayContent').append('<p>' + value + " at " + dayTemperature + "°C" + '<p>');
-            }
-            //check if there are already 5 switches if so, no button to add is displayed
-            if (obj['Wednesday'].length < 5) {
-                $('#WednesdayContent').append('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+                $('#WednesdayContent').prepend('<p>' + value + " at " + dayTemperature + "°C" + '</p>');
             }
         }
 
         if (index == 'Thursday') {
+            //check if there are already 5 switches if so, no button to add is displayed
+            if (obj['Thursday'].length < 5) {
+                $('#ThursdayContent').prepend('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+            }
             for (i = 0; i < obj['Thursday'].length; i++) {
                 var value = obj['Thursday'][i].toString().replace(",", " - ");
 
-                $('#ThursdayContent').append('<p>' + value + " at " + dayTemperature + "°C" + '<p>');
+                $('#ThursdayContent').prepend('<p>' + value + " at " + dayTemperature + "°C" + '</p>');
             }
-            //check if there are already 5 switches if so, no button to add is displayed
-            if (obj['Thursday'].length < 5) {
-                $('#ThursdayContent').append('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
-            }
+
         }
 
         if (index == 'Friday') {
-            for (i = 0; i < obj['Friday'].length; i++) {
-                var value = obj['Friday'][i].toString().replace(",", " - ");
-
-                $('#FridayContent').append('<p>' + value + " at " + dayTemperature + "°C" + '<p>');
-            }
             //check if there are already 5 switches if so, no button to add is displayed
             if (obj['Friday'].length < 5) {
-                $('#FridayContent').append('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+                $('#FridayContent').prepend('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+            }
+            for (i = 0; i < obj['Friday'].length; i++) {
+                var value = obj['Friday'][i].toString().replace(",", " - ");
+                $('#FridayContent').prepend('<p>' + value + " at " + dayTemperature + "°C" + '</p>');
             }
         }
 
         if (index == 'Saturday') {
+            //check if there are already 5 switches if so, no button to add is displayed
+            if (obj['Saturday'].length < 5) {
+                $('#SaturdayContent').prepend('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+            }
             for (i = 0; i < obj['Saturday'].length; i++) {
                 var value = obj['Saturday'][i].toString().replace(",", " - ");
 
-                $('#SaturdayContent').append('<p>' + value + " at " + dayTemperature + "°C" + '<p>');
-            }
-            //check if there are already 5 switches if so, no button to add is displayed
-            if (obj['Saturday'].length < 5) {
-                $('#SaturdayContent').append('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+                $('#SaturdayContent').prepend('<p>' + value + " at " + dayTemperature + "°C" + '</p>');
             }
         }
 
         if (index == 'Sunday') {
+            //check if there are already 5 switches if so, no button to add is displayed
+            if (obj['Sunday'].length < 5) {
+                $('#SundayContent').prepend('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+            }
             for (i = 0; i < obj['Sunday'].length; i++) {
                 var value = obj['Sunday'][i].toString().replace(",", " - ");
 
-                $('#SundayContent').append('<p>' + value + " at " + dayTemperature + "°C" + '<p>');
-            }
-            //check if there are already 5 switches if so, no button to add is displayed
-            if (obj['Sunday'].length < 5) {
-                $('#SundayContent').append('<span class="btn btn-primary addSwitchBtn">Add a switch</span>');
+                $('#SundayContent').prepend('<p>' + value + " at " + dayTemperature + "°C" + '</p>');
             }
         }
     });
@@ -106,9 +105,21 @@ $(document).ready(function () {
         var day = parentID.replace("Content", "");
         var from = $('#fromInput').val();
         var to = $('#toInput').val();
-        console.log(day + fromr + to);
+        console.log(day + from + to);
         addPeriod(day, from, to);
+        $('#fromToContainer').css('visibility', 'hidden');
         //Need a refresh to show the just added data. make a function out of it
+    });
+    
+    //to delete a switch with id nr. i
+    $('body').on('click', '.removeBtn', function () {
+        var i = $(".removeBtn").closest("span").attr("id"); 
+        removePeriod('Monday', i);
+    });
+
+    //to show switch input
+    $('body').on('click', '.addSwitchBtn', function () {
+        $('#fromToContainer').css('visibility', 'visible');
     });
 
 });
